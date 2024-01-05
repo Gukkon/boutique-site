96fb45a67c88dafb19068490cd8ba5c32af19094
Shaun Homer <shaunhomer1@hotmail.com>
2024-01-05 18:13:07 +0000

    stripe added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OVHT0I2b26oPL7jHefyaPsoIu1BE97jLZEb49LmipBexuH9aqAMYKaBhhQDdJPwBmcdYwNPldaLJIqjdzvRLo8600dm4cFWm2',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
