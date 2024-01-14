d0751bbbd35b122609db110764b899c4e3b80154
Gukkon <shaunhomer1@hotmail.com>
2024-01-14 15:57:26 +0000

    remove secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--19w+mf9j4#ef3a-cr#gp7o_acu%y^jgc51ux_ubw(&!t1c&!f'
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['boutiqueado77-bd5d834f1887.herokuapp.com']
+ALLOWED_HOSTS = ['boutiqueado77-bd5d834f1887.herokuapp.com',
+                'localhost',
+                '8000-gukkon-boutiquesite-r9xgg4vezzl.ws-eu107.gitpod.io'
+]
 
 
 # Application definition
