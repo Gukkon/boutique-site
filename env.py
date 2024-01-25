dd179881444a152bee9d381e8168ed5b9567c949
Gukkon <shaunhomer1@hotmail.com>
2024-01-25 18:38:51 +0000

    finally sorted

--- env.py
+++ env.py
@@ -1,5 +1,8 @@
 import os
 
+os.environ["USE_AWS"] = "True"
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIA5FTY6MIDMBTDU4EL"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
 os.environ["SECRET_KEY"] = "Yarmouth"
 os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51OVHT0I2b26oPL7jHefyaPsoIu1BE97jLZEb49LmipBexuH9aqAMYKaBhhQDdJPwBmcdYwNPldaLJIqjdzvRLo8600dm4cFWm2"
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51OVHT0I2b26oPL7jD00Ui98GW5MAkk9en6LEIIxhZ8drCTErB1QVEYUisjh6ABYEgypOuvhL9Tzg3ykdgE4OHkf900FU7LmtIx"
